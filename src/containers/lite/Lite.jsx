92dcc8af5941e81ebf07b040c60110c0dda4810e
Husain Bhagat <hussainbhagat52@gmail.com>
2022-08-14 12:20:48 +0530

    Feat: Add Lite Section

--- src/containers/lite/Lite.jsx
+++ src/containers/lite/Lite.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import './lite.css'
+import React from "react";
+import "./lite.css";
+import liteCard from "../../assets/tapi lite.svg";
+import arrow from "../../assets/arrow.svg";
 
 const Lite = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="lite">
+      <div className="lite__info">
+        <div className="lite__info-header">
+          <h1>TAPI</h1>
+          <h1 className="text-stroke">LITE</h1>
         </div>
-    )
-}
+        <div className="lite__info-dimension">
+          <p>Weight : 0.10gms</p>
+          <p>Size : 85.5mm X 54mm X 0.84mm</p>
+          <p>Frequency: 13.56Hz</p>
+          <span>Recommended for students</span>
+        </div>
+        <div className="apply-button">
+          <button className="apply bg-orange">
+            <div className="apply-text">
+              Apply Now <img src={arrow} alt="" />
+            </div>
+          </button>
+          <div className="apply-rectangle border-orange"></div>
+        </div>
+      </div>
+      <div className="lite-card">
+        <img src={liteCard} alt="" className="lite-card-svg" />
+      </div>
+    </div>
+  );
+};
 
-export default Lite
+export default Lite;
--- src/containers/options/Options.jsx
+++ src/containers/options/Options.jsx
@@ -1,56 +1,59 @@
 import React from "react";
 import "./options.css";
-import handCard from '../../assets/handCard.svg';
-import { Controller, Scene } from 'react-scrollmagic';
-
+import handCard from "../../assets/handCard.svg";
+import { Controller, Scene } from "react-scrollmagic";
 
 const Options = () => {
   return (
     <Controller>
-     
-    <div className="options">
-      <div className="options__header">
-          <h1>
-          Anywhere. Anytime. Anyone.
-          </h1>
-          </div>
-          <div className="options__section">
-      <div className="options__header-options">
-        <div className="options__header-option">
-          <div className="options__sideline"></div>
-          <div className="options__content">
-          <h2>For Students</h2>
-          <p>
-            Everyone knows you’re a student, but your card will help people
-            remember what you studied.
-          </p>
-          </div>
+      <div className="options">
+        <div className="options__header">
+          <h1>Anywhere. Anytime. Anyone.</h1>
         </div>
-        <div className="options__header-option">
-          <div className="options__sideline"></div>
-          <div className="options__content">
-          <h2>For Freelancers</h2>
-          <p>
-            First impression should be your best impression for your upcoming
-            project!
-          </p>
+        <div className="options__section">
+          <div className="options__header-options">
+            <div className="options__header-option">
+              <div className="options__sideline"></div>
+              <div className="options__content">
+                <h2>For Students</h2>
+                <p>
+                  Everyone knows you’re a student, but your card will help
+                  people remember what you studied.
+                </p>
+              </div>
+            </div>
+            <div className="options__header-option">
+              <div className="options__sideline"></div>
+              <div className="options__content">
+                <h2>For Freelancers</h2>
+                <p>
+                  First impression should be your best impression for your
+                  upcoming project!
+                </p>
+              </div>
+            </div>
+            <div className="options__header-option">
+              <div className="options__sideline"></div>
+              <div className="options__content">
+                <h2>For Professionals</h2>
+                <p>Make winning business connections easy</p>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="options__header-option">
-          <div className="options__sideline"></div>
-          <div className="options__content">
-          <h2>For Professionals</h2>
-          <p>Make winning business connections easy</p>
-          </div>
+          <Scene
+            indicators={false}
+            triggerHook="0"
+            offset={-200}
+            duration="30%"
+            pin={true}
+            enabled={true}
+          >
+            <div className="options__card">
+              <img src={handCard} alt="Card" className="options__card-hand" />
+            </div>
+          </Scene>
         </div>
       </div>
-      <Scene indicators={true} triggerHook="0" offset={-200} duration="30%" pin={true} enabled={true} >
-      <div className="options__card">
-          <img src={handCard} alt="Card" className="options__card-hand"/>
-      </div>
-    </Scene>
-          </div>
-    </div>
     </Controller>
   );
 };
